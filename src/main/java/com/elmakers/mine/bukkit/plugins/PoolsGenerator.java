884b7fa1f13f31e41c4d54b13f5be5a9b5e4f541
Nathan Wolf <nathan@elmakers.com>
2026-05-08 20:59:14 +0200

    Update pools to be more pools like

--- src/main/java/com/elmakers/mine/bukkit/plugins/PoolsGenerator.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/PoolsGenerator.java
@@ -1,48 +1,62 @@
 package com.elmakers.mine.bukkit.plugins;
 
 import org.bukkit.Material;
+import org.bukkit.generator.BiomeProvider;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
 public class PoolsGenerator extends ChunkGenerator {
-    private static final int SEA_LEVEL = 64;
-    private static final int SAND_LEVEL = 6;
-    private static final int BEDROCK_LAYER = 1;
-    private static final int ISLAND_LAYER = 60;
+    private static final int BEDROCK_LEVEL = 60;
+    private static final int ROOF_HEIGHT = 5;
     private final LiminalWorldPlugin plugin;
+    private final BiomeProvider biomeProvider;
 
     public PoolsGenerator(LiminalWorldPlugin plugin) {
         this.plugin = plugin;
+        biomeProvider = new DesertBiomeProvider();
+    }
+
+    @Nullable
+    public BiomeProvider getDefaultBiomeProvider(@NotNull WorldInfo worldInfo) {
+        return biomeProvider;
     }
 
     @Override
     public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkData chunk) {
+        final int floorLevel = BEDROCK_LEVEL + 2;
+        final int roofLevel = floorLevel + ROOF_HEIGHT;
+
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                for (int y = SEA_LEVEL; y > ISLAND_LAYER; y--) {
-
-                    boolean ocean =true;
-                    int wx = Math.abs(x+chunkX*16);
-                    int wz = Math.abs(z+chunkZ*16);
-                    wx = wx % 500;
-                    wz = wz % 500;
-                    if (wx>450 && wx<499 && wz>450 && wz<499)
-                        ocean=false;
-                    if (ocean)
-                        chunk.setBlock(x, y, z, Material.WATER);
-                    else
-                        chunk.setBlock(x, y, z, Material.SMOOTH_SANDSTONE);
+                if (x == 0 || z == 0) {
+                    // Walls and doorway
+                    boolean isDoorway = x == 7 || z == 7 || x == 8 || z == 8 || x == 9 || z == 9;
+                    for (int y = floorLevel; y <= roofLevel; y++) {
+                        if (isDoorway && y < floorLevel + 3 && y > floorLevel) continue;
+                        chunk.setBlock(x, y, z, Material.QUARTZ_BLOCK);
+                    }
+                } else if (x == 1 || z == 1 || x == 15 || z == 15) {
+                    // Walkway
+                    chunk.setBlock(x, floorLevel, z, Material.QUARTZ_BLOCK);
+                    chunk.setBlock(x, roofLevel, z, Material.QUARTZ_BLOCK);
+                } else if (x >= 7 && z >= 7 && x <= 9 && z <= 9) {
+                    // Island
+                    chunk.setBlock(x, floorLevel, z, Material.QUARTZ_BLOCK);
+                } else if (x == 8 || z == 8) {
+                    // Pathways
+                    chunk.setBlock(x, roofLevel, z, Material.QUARTZ_BLOCK);
+                    chunk.setBlock(x, floorLevel, z, Material.QUARTZ_BLOCK);
+                } else {
+                    // Water and roof
+                    chunk.setBlock(x, roofLevel, z, Material.QUARTZ_BLOCK);
+                    chunk.setBlock(x, floorLevel, z, Material.WATER);
                 }
-                for (int y = ISLAND_LAYER; y > SAND_LEVEL; y--) {
-                    chunk.setBlock(x, y, z, Material.WATER);
-                }
-                for (int y = SAND_LEVEL; y >= BEDROCK_LAYER; y--) {
-                    chunk.setBlock(x, y, z, Material.SAND);
-                }
-                chunk.setBlock(x, 0, z, Material.BEDROCK);
+                chunk.setBlock(x, BEDROCK_LEVEL + 1, z, Material.QUARTZ_BLOCK);
+                chunk.setBlock(x, BEDROCK_LEVEL, z, Material.BEDROCK);
             }
         }
     }
