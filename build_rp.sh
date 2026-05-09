e93ecef4ff3d06492583fb55a15301d35e91171d
Nathan Wolf <nathan@elmakers.com>
2026-05-09 15:41:35 +0200

    Add resource pack

---
+++ build_rp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "$( dirname "$0" )"
+cd target
+rm -Rf rp
+mkdir rp
+cd rp
+
+echo "** BUILDING RP **"
+
+cp -R ../../src/rp/* .
+
+# Clean and zip
+find . -name ".DS_Store" -type f -delete
+zip -q -X -r ../LiminalDeep-RP.zip *
+cd ..
+
+echo "** COPYING TO MINECRAFT **"
+cp *.zip ~/Library/Application\ Support/minecraft/resourcepacks/
