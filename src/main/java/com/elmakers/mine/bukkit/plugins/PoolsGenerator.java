76cbf9aa3364cd3f412859e52bdb0be4be40e7fd
Nathan Wolf <nathan@elmakers.com>
2026-05-09 14:27:08 +0200

    Randomize door sizes

--- src/main/java/com/elmakers/mine/bukkit/plugins/PoolsGenerator.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/PoolsGenerator.java
@@ -12,8 +12,11 @@ import java.util.Random;
 public class PoolsGenerator extends ChunkGenerator {
     private static final int BEDROCK_LEVEL = 60;
     public static final int FLOOR_LEVEL = BEDROCK_LEVEL + 2;
-    private static final int ROOF_MIN_HEIGHT = 3;
+    private static final int ROOF_MIN_HEIGHT = 4;
     private static final int ROOF_MAX_HEIGHT = 10;
+    private static final int DOORWAY_MIN_HEIGHT = 2;
+    private static final int DOORWAY_MAX_HEIGHT = 4;
+    private static final int DOORWAY_MAX_WIDTH_HALF = 3;
     private static final double WALL_PROBABILITY = 0.91;
     private final LiminalWorldPlugin plugin;
     private final BiomeProvider biomeProvider;
@@ -32,7 +35,11 @@ public class PoolsGenerator extends ChunkGenerator {
     public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkData chunk) {
         final int floorLevel = FLOOR_LEVEL;
         final int roofLevel = floorLevel + random.nextInt(ROOF_MAX_HEIGHT - ROOF_MIN_HEIGHT) + ROOF_MIN_HEIGHT;
-        final int roofMaxLevel = roofLevel + ROOF_MAX_HEIGHT;
+        final int roofMaxLevel = floorLevel + ROOF_MAX_HEIGHT;
+        final int doorwayLevel = Math.min(roofLevel, floorLevel + random.nextInt(DOORWAY_MAX_HEIGHT - DOORWAY_MIN_HEIGHT) + DOORWAY_MIN_HEIGHT);
+        final int doorwayWidthHalf = random.nextInt(DOORWAY_MAX_WIDTH_HALF);
+        final int doorwayLeft = 7 - doorwayWidthHalf;
+        final int doorwayRight = 9 + doorwayWidthHalf;
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
                 if (x == 0 || z == 0) {
@@ -40,9 +47,9 @@ public class PoolsGenerator extends ChunkGenerator {
                     boolean hasWall = true;
                     if (hasWall) {
                         // Walls and doorway
-                        boolean isDoorway = x == 7 || z == 7 || x == 8 || z == 8 || x == 9 || z == 9;
+                        boolean isDoorway = (x >= doorwayLeft && x <= doorwayRight) || (z >= doorwayLeft && z <= doorwayRight);
                         for (int y = floorLevel; y <= roofMaxLevel; y++) {
-                            if (isDoorway && y < floorLevel + 3 && y > floorLevel) continue;
+                            if (isDoorway && y <= doorwayLevel && y > floorLevel) continue;
                             chunk.setBlock(x, y, z, Material.QUARTZ_BLOCK);
                         }
                     } else {
